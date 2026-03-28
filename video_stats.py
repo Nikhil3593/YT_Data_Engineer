6b543e972547c451cc50e26db260467a20257733
nikhil3593 <nikhil3k@gmail.com>
2026-03-28 17:15:10 -0400

    feat:python pull api function

--- video_stats.py
+++ video_stats.py
@@ -1,6 +1,24 @@
 import requests
-
-API_KEY="AIzaSyCwrfliF_ELfKxD6bRTN4dRjU1LBKHikz0"
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="./.env")
+API_KEY=os.getenv("API_KEY")
 CHANNEL_HANDLE="MrBeast"
 
-url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentdetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+def get_playlist_id():
+    try:
+        url=f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+        response = requests.get(url)
+        response.raise_for_status()  # Check if the request was successful  
+        data = response.json()
+        #print(json.dumps(data, indent=4))
+        channel_items=data['items'][0]
+        channel_playlistId=channel_items['contentDetails']['relatedPlaylists']['uploads']
+        print(channel_playlistId)
+        return channel_playlistId
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    get_playlist_id()
